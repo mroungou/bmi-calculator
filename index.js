c085ecc7f8f3bf69b794ecb9a17d821b064619e5
Malick Roungou <mjbroungou@gmail.com>
2024-01-29 11:42:35 -0500

    calculator complet

--- index.js
+++ index.js
@@ -3,22 +3,60 @@ const form = document.getElementById('calculator');
 const metricButton = document.getElementById('metric');
 const imperialButton = document.getElementById('imperial');
 // inputs divs
-const ImperialInputsDiv = document.getElementsByClassName('imperial-sys')
-const metricInputsDiv = document.getElementsByClassName('metric-sys')
+const ImperialInputsDiv = document.getElementsByClassName('imperial-sys');
+const metricInputsDiv = document.getElementsByClassName('metric-sys');
+// input values for metric
+const weightKG = document.getElementById('weight-kg');
+const heightCM = document.getElementById('height-cm');
 
+// input values for imperial
+const weightStones = document.getElementById('weight-st');
+const weightLBS = document.getElementById('weight-lbs');
+const heightFT = document.getElementById('height-ft');
+const heightIN = document.getElementById('height-in');
+
+const BMIScore = document.getElementById('score');
+let BMI
+
+// calculating BMI
+function calculateBMIMetric() {
+    const weightValue = parseFloat(weightKG.value);
+    const heightValue = parseFloat(heightCM.value);
+    // span element that contains the number
+
+    BMI = (weightValue / heightValue ** 2) * 10000;
+    const BMIRounded = BMI.toFixed(1)
+    BMIScore.innerText = BMIRounded;
+}
+
+function calculateBMIImperial () {
+    const weightLBSValue = parseFloat(weightLBS.value);
+    const weightStonesValue = parseFloat(weightStones.value);
+    const heightFTValue = parseFloat(heightFT.value);
+    const heightINValue = parseFloat(heightIN.value);
+
+    const totalWeight = (weightStonesValue * 14) + weightLBSValue
+    const totalHeight = (heightFTValue * 12) + heightINValue
+
+    BMI = (totalWeight / totalHeight ** 2) * 703
+    const BMIRounded = BMI.toFixed(1)
+    BMIScore.innerText = BMIRounded
+}
 
 function displayMetric() {
-    for (let i = 0; i < metricInputsDiv.length; i++) {
-        metricInputsDiv[i].classList.add('visible');
-    }
-    for (let i = 0; i < ImperialInputsDiv.length; i++) {
-        ImperialInputsDiv[i].classList.remove('visible')
-        ImperialInputsDiv[i].classList.add('hidden')
+    if (metricButton.checked == true) { //need to review this, may be useless
+        for (let i = 0; i < metricInputsDiv.length; i++) {
+            metricInputsDiv[i].classList.add('visible');
+        }
+        for (let i = 0; i < ImperialInputsDiv.length; i++) {
+            ImperialInputsDiv[i].classList.remove('visible')
+            ImperialInputsDiv[i].classList.add('hidden')
+        }
     }
 }
 
 function displayImperial() {
-    if (imperialButton.checked == true) {
+    if (imperialButton.checked == true) { //need to review this, may be useless
         for (let i = 0; i < ImperialInputsDiv.length; i++) {
             ImperialInputsDiv[i].classList.add('visible');
         }
@@ -29,5 +67,13 @@ function displayImperial() {
     }
 }
 
-imperialButton.addEventListener('click', displayImperial)
-metricButton.addEventListener('click', displayMetric)
+imperialButton.addEventListener('click', displayImperial);
+metricButton.addEventListener('click', displayMetric);
+
+weightKG.addEventListener('keyup', calculateBMIMetric);
+heightCM.addEventListener('keyup', calculateBMIMetric);
+
+weightStones.addEventListener('keyup', calculateBMIImperial)
+weightLBS.addEventListener('keyup', calculateBMIImperial)
+heightFT.addEventListener('keyup', calculateBMIImperial)
+heightIN.addEventListener('keyup', calculateBMIImperial)
